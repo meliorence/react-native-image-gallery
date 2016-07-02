3d738e8399432818643f46cb9b67a4cbb5d70350
ldn0x7dc <ldn0x7dc@gmail.com>
2016-07-02 14:03:14 +0800

    Update dependency; Update demo

--- Demo/App.js
+++ Demo/App.js
@@ -16,24 +16,12 @@ export default class App extends Component {
     return (
       <Gallery
         style={{flex: 1, backgroundColor: 'black'}}
-        initialPage={3}
+        initialPage={1}
         pageMargin={10}
         images={[
           'http://p10.qhimg.com/t019e9cf51692f735be.jpg',
           'http://ww2.sinaimg.cn/mw690/714a59a7tw1dxqkkg0cwlj.jpg',
-          'http://www.bz55.com/uploads/allimg/150122/139-150122145421.jpg',
-          'http://p10.qhimg.com/t019e9cf51692f735be.jpg',
-          'http://ww2.sinaimg.cn/mw690/714a59a7tw1dxqkkg0cwlj.jpg',
-          'http://www.bz55.com/uploads/allimg/150122/139-150122145421.jpg',
-          'http://p10.qhimg.com/t019e9cf51692f735be.jpg',
-          'http://ww2.sinaimg.cn/mw690/714a59a7tw1dxqkkg0cwlj.jpg',
-          'http://www.bz55.com/uploads/allimg/150122/139-150122145421.jpg',
-          'http://p10.qhimg.com/t019e9cf51692f735be.jpg',
-          'http://ww2.sinaimg.cn/mw690/714a59a7tw1dxqkkg0cwlj.jpg',
-          'http://www.bz55.com/uploads/allimg/150122/139-150122145421.jpg',
-          'http://p10.qhimg.com/t019e9cf51692f735be.jpg',
-          'http://ww2.sinaimg.cn/mw690/714a59a7tw1dxqkkg0cwlj.jpg',
-          'http://www.bz55.com/uploads/allimg/150122/139-150122145421.jpg'
+          //'http://www.bz55.com/uploads/allimg/150122/139-150122145421.jpg'
         ]}
       />
     );
