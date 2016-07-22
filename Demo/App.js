2b88a6b4c6bd18617e859abd827a2f953fa01a93
ldn0x7dc <ldn0x7dc@gmail.com>
2016-07-22 10:49:25 +0800

    Update Demo

--- Demo/App.js
+++ Demo/App.js
@@ -5,7 +5,8 @@ import {
   Text,
   View,
   Dimensions,
-  ScrollView
+  ScrollView,
+  Modal
 } from 'react-native';
 
 import Gallery from 'react-native-gallery';
@@ -34,31 +35,35 @@ export default class App extends Component {
     }
 
     return (
-      <View style={{flex: 1}}>
-        <Gallery
-          style={{flex: 1, backgroundColor: 'black'}}
-          initialPage={1}
-          pageMargin={10}
-          images={[
+      <Modal
+        visible={true}
+      >
+        <View style={{flex: 1}}>
+          <Gallery
+            style={{flex: 1, backgroundColor: 'black'}}
+            initialPage={1}
+            pageMargin={10}
+            images={[
           'http://p10.qhimg.com/t019e9cf51692f735be.jpg',
           'http://ww2.sinaimg.cn/mw690/714a59a7tw1dxqkkg0cwlj.jpg',
           'http://www.bz55.com/uploads/allimg/150122/139-150122145421.jpg'
         ]}
-          onSingleTapConfirmed={() => {
+            onSingleTapConfirmed={() => {
           this.toggleCommentBox();
         }}
-          onGalleryStateChanged={(idle) => {
+            onGalleryStateChanged={(idle) => {
           if(!idle) {
             this.hideCommentBox();
           }
         }}
-          onPageSelected={(page) => {
+            onPageSelected={(page) => {
            this.setState({page});
         }}
-        />
+          />
 
-        {commentBox}
-      </View>
+          {commentBox}
+        </View>
+      </Modal>
     );
   }
 
