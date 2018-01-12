33a94a8760b38aae4939eaf79cf4f30add043f0d
Maxime Bertonnier <m.bertonnier@gmail.com>
2018-01-12 15:32:55 +0100

    chore(GestureResponder): lint baked-in dependency

--- src/libraries/GestureResponder/TouchHistoryMath.js
+++ src/libraries/GestureResponder/TouchHistoryMath.js
@@ -21,79 +21,79 @@ var TouchHistoryMath = {
    * touches vs. previous centroid of now actively moving touches.
    * @return {number} value of centroid in specified dimension.
    */
-  centroidDimension: function (touchHistory, touchesChangedAfter, isXAxis, ofCurrent) {
-    var touchBank = touchHistory.touchBank;
-    var total = 0;
-    var count = 0;
+    centroidDimension: function (touchHistory, touchesChangedAfter, isXAxis, ofCurrent) {
+        var touchBank = touchHistory.touchBank;
+        var total = 0;
+        var count = 0;
 
-    var oneTouchData = touchHistory.numberActiveTouches === 1 ? touchHistory.touchBank[touchHistory.indexOfSingleActiveTouch] : null;
+        var oneTouchData = touchHistory.numberActiveTouches === 1 ? touchHistory.touchBank[touchHistory.indexOfSingleActiveTouch] : null;
 
-    if (oneTouchData !== null) {
-      if (oneTouchData.touchActive && oneTouchData.currentTimeStamp > touchesChangedAfter) {
-        total += ofCurrent && isXAxis ? oneTouchData.currentPageX : ofCurrent && !isXAxis ? oneTouchData.currentPageY : !ofCurrent && isXAxis ? oneTouchData.previousPageX : oneTouchData.previousPageY;
-        count = 1;
-      }
-    } else {
-      for (var i = 0; i < touchBank.length; i++) {
-        var touchTrack = touchBank[i];
-        if (touchTrack !== null && touchTrack !== undefined && touchTrack.touchActive && touchTrack.currentTimeStamp >= touchesChangedAfter) {
-          var toAdd; // Yuck, program temporarily in invalid state.
-          if (ofCurrent && isXAxis) {
-            toAdd = touchTrack.currentPageX;
-          } else if (ofCurrent && !isXAxis) {
-            toAdd = touchTrack.currentPageY;
-          } else if (!ofCurrent && isXAxis) {
-            toAdd = touchTrack.previousPageX;
-          } else {
-            toAdd = touchTrack.previousPageY;
-          }
-          total += toAdd;
-          count++;
+        if (oneTouchData !== null) {
+            if (oneTouchData.touchActive && oneTouchData.currentTimeStamp > touchesChangedAfter) {
+                total += ofCurrent && isXAxis ? oneTouchData.currentPageX : ofCurrent && !isXAxis ? oneTouchData.currentPageY : !ofCurrent && isXAxis ? oneTouchData.previousPageX : oneTouchData.previousPageY;
+                count = 1;
+            }
+        } else {
+            for (var i = 0; i < touchBank.length; i++) {
+                var touchTrack = touchBank[i];
+                if (touchTrack !== null && touchTrack !== undefined && touchTrack.touchActive && touchTrack.currentTimeStamp >= touchesChangedAfter) {
+                    var toAdd; // Yuck, program temporarily in invalid state.
+                    if (ofCurrent && isXAxis) {
+                        toAdd = touchTrack.currentPageX;
+                    } else if (ofCurrent && !isXAxis) {
+                        toAdd = touchTrack.currentPageY;
+                    } else if (!ofCurrent && isXAxis) {
+                        toAdd = touchTrack.previousPageX;
+                    } else {
+                        toAdd = touchTrack.previousPageY;
+                    }
+                    total += toAdd;
+                    count++;
+                }
+            }
         }
-      }
-    }
-    return count > 0 ? total / count : TouchHistoryMath.noCentroid;
-  },
+        return count > 0 ? total / count : TouchHistoryMath.noCentroid;
+    },
 
-  currentCentroidXOfTouchesChangedAfter: function (touchHistory, touchesChangedAfter) {
-    return TouchHistoryMath.centroidDimension(touchHistory, touchesChangedAfter, true, // isXAxis
+    currentCentroidXOfTouchesChangedAfter: function (touchHistory, touchesChangedAfter) {
+        return TouchHistoryMath.centroidDimension(touchHistory, touchesChangedAfter, true, // isXAxis
     true // ofCurrent
     );
-  },
+    },
 
-  currentCentroidYOfTouchesChangedAfter: function (touchHistory, touchesChangedAfter) {
-    return TouchHistoryMath.centroidDimension(touchHistory, touchesChangedAfter, false, // isXAxis
+    currentCentroidYOfTouchesChangedAfter: function (touchHistory, touchesChangedAfter) {
+        return TouchHistoryMath.centroidDimension(touchHistory, touchesChangedAfter, false, // isXAxis
     true // ofCurrent
     );
-  },
+    },
 
-  previousCentroidXOfTouchesChangedAfter: function (touchHistory, touchesChangedAfter) {
-    return TouchHistoryMath.centroidDimension(touchHistory, touchesChangedAfter, true, // isXAxis
+    previousCentroidXOfTouchesChangedAfter: function (touchHistory, touchesChangedAfter) {
+        return TouchHistoryMath.centroidDimension(touchHistory, touchesChangedAfter, true, // isXAxis
     false // ofCurrent
     );
-  },
+    },
 
-  previousCentroidYOfTouchesChangedAfter: function (touchHistory, touchesChangedAfter) {
-    return TouchHistoryMath.centroidDimension(touchHistory, touchesChangedAfter, false, // isXAxis
+    previousCentroidYOfTouchesChangedAfter: function (touchHistory, touchesChangedAfter) {
+        return TouchHistoryMath.centroidDimension(touchHistory, touchesChangedAfter, false, // isXAxis
     false // ofCurrent
     );
-  },
+    },
 
-  currentCentroidX: function (touchHistory) {
-    return TouchHistoryMath.centroidDimension(touchHistory, 0, // touchesChangedAfter
+    currentCentroidX: function (touchHistory) {
+        return TouchHistoryMath.centroidDimension(touchHistory, 0, // touchesChangedAfter
     true, // isXAxis
     true // ofCurrent
     );
-  },
+    },
 
-  currentCentroidY: function (touchHistory) {
-    return TouchHistoryMath.centroidDimension(touchHistory, 0, // touchesChangedAfter
+    currentCentroidY: function (touchHistory) {
+        return TouchHistoryMath.centroidDimension(touchHistory, 0, // touchesChangedAfter
     false, // isXAxis
     true // ofCurrent
     );
-  },
+    },
 
-  noCentroid: -1
+    noCentroid: -1
 };
 
 module.exports = TouchHistoryMath;
